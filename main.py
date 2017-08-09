ffc4d710dae9e44019fbb5d0bafe03eff3e4cc6f
attilaballa <xattus@gmail.com>
2017-08-09 17:50:26 +0200

    backend: saving to CSV works

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, session
+import csv
 
 app = Flask(__name__)
 app.secret_key = 'ITSSOSECRETDONTTELLANYONE'
@@ -9,6 +10,20 @@ def get_resource_as_string(name, charset='utf-8'):
         return f.read().decode(charset)
 
 app.jinja_env.globals['get_resource_as_string'] = get_resource_as_string
+
+
+def savetocsv(dictionary):
+    FORM_ITEMS = ['title', 'userstory', 'acceptance', 'status']  # sets the order of saving items
+    output = []
+    for item in FORM_ITEMS:
+        if(item in dictionary.keys()):
+            output.append(dictionary[item])
+        else:
+            return False
+
+    writer = csv.writer(open('storydata.csv', 'w'))
+    writer.writerow(output)
+    return True
 
 
 @app.route('/')
@@ -23,10 +38,18 @@ def route_edit():
 
 @app.route('/save-story', methods=['POST'])
 def route_save():
+    result = {}
     print('POST request received!')
     formdata = request.form
-    print(formdata)
-    return redirect('/')
+
+    for item in formdata.items():
+        result[item[0]] = item[1]
+
+    if(savetocsv(result)):
+        print('Data saved successfully!')
+    else:
+        print('An error occured when saving the data!')
+    return redirect('/story')
 
 
 if __name__ == "__main__":
